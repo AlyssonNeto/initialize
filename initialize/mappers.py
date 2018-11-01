99d63963c210cead6432fe6ebc3e8999093793eb
Akash Chaudhari <devilscyanide@gmail.com>
2018-11-01 19:14:50 +0530

    Added wordpress theme file

---
+++ initialize/mappers.py
@@ -0,0 +1,53 @@
+"""
+This is Mapper module contains array of file to create
+"""
+
+# Wordpress Theme Structure
+WORDPRESS_THEME = [{'assets':[{'css': []},
+							  {'images': []},
+							  {'js': []}
+							 ]},
+					{'inc': []},
+					{'template-parts': [{'footer': []},
+										{'header': []},
+										{'navigation': []},
+										{'page': []},
+										{'post': []}
+										]},
+					{'languages': []},
+					'404.php',
+					'archive.php',
+					'comments.php',
+					'footer.php',
+					'front-page.php',
+					'functions.php',
+					'header.php',
+					'index.php',
+					'page.php',
+					'Readme.txt',
+					'rtl.css',
+					'screenshot.png',
+					'search.php',
+					'searchform.php',
+					'sidebar.php',
+					'single.php',
+					'style.css']
+
+
+# Python Standard Project
+# Project directory will be created directly
+
+PYTHON_PROJ = ['setup.py',
+			   'setup.cfg',
+			   'tox.ini',
+			   '.gitattributes',
+			   '.gitignore',
+			   'Authors',
+			   'Contributing',
+			   {'{0}':['_compat.py']},
+			   'License',
+			   {'tests':[]},
+			   {'docs':[]}]
+
+
+
